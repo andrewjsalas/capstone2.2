4cca4039c31e0031071a848486131aaf435f17cf
asalas <67822688+andrewjsalas@users.noreply.github.com>
2023-05-14 16:43:58 -0700

    Finish user routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -50,3 +50,153 @@ router.get('/:id', async (req, res) => {
         res.status(500).send({ error: e.message });
     }
 });
+
+//Creates new user with the provided username and password. It first checks if password and username are present and if so, hashes the password using bcrypt with a cost factor of 10, then inserts it into the 'users' table. 
+router.post('/', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        if (!username) {
+            throw new Error('Username is required');
+        }
+        if (!password) {
+            throw new Error('Password is required');
+        }
+
+        const hashedPassword = await bcrypt.hash(passowrd, 10);
+
+        const insertIntoUserStatement = `
+        INSERT INTO users(username, password)
+        VALUES ($1, $2)
+        RETURNING *
+        `
+
+        let rows;
+        try {
+            ({ rows }) = await query(insertIntoUserStatement, [username, hashedPassword]);
+        } catch (e) {
+            res.status(409).send({ error: 'Username is already taken. '});
+        }
+
+        const { user, token } = await addToken(rows[0].id);
+
+        res.status(201).send({
+            user: getPublicUser(user),
+            token
+        })
+    } catch (e) {
+        res.status(400).send({ error: e.message });
+    }
+});
+
+// User login 
+router.get('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        if (!username || !password) {
+            throw new Error('Username and password are required.');
+        }
+
+        const selectUserStatement = `
+        SELECT *
+        FROM users
+        WHERE username = $1
+        `
+        const { rows } = await query(selectUserStatement, [username]);
+        const failedLoginError = { error: 'Username or password was incorrect.'};
+
+        // If no auth match, throw error
+        if (!rows[0]) {
+            return res.status(401).send(failedLoginError);
+        }
+
+        // Compares input password with logged hashed password
+        const isMatch = await bcrypt.compare(password, rows[0].password);
+        if (!isMatch) {
+            return res.status(401).send(failedLoginError);
+        }
+
+        const { user, token } = await addToken(rows[0].id);
+
+        res.send({
+            user: getPublicUser(user),
+            token
+        });
+    } catch (e) {
+        res.status(400).send({ error: e.message });
+    }
+});
+
+// Logout user
+router.post('/logout', auth, async (req, res) => {
+    const tokens = req.user.tokens.filter((token) => token != req.token);
+    const setUserTokensStatement = `
+    UPDATE users
+    SET tokens = $1
+    WHERE id = $2
+    `
+    const { rows: [user] } = await query(setUserTokensStatement, [tokens, req.user.id]);
+    delete req.user;
+    delete req.token;
+    res.send(user);
+});
+
+router.post('/logoutAll', auth, async (req, res) => {
+    const clearUserTokensStatement = `
+    UPDATE users
+    SET tokens = '{}'
+    WHERE id = $1
+    `
+    const { rows: [user] } = await query(clearUserTokensStatement, [req.user.id]);
+    delete req.user
+    delete req.token
+    res.send(user);
+});
+
+// Update user
+router.put('/', auth, async (req, res) => {
+    try {
+        const allowedUpdates = ['username', 'password'];
+        if (req.body.username !== undefined) {
+            const { rows } = await query(`
+                SELECT *
+                FROM users
+                WHERE username = $1
+                `,
+                [req.body.username]
+            );
+            if (rows.length > 0) {
+                return res.status(409).send({ error: 'Username is already taken.' })
+            }
+        }
+
+        if (req.body.password !== undefined) {
+            req.body.password = await bcrypt.hash(req.body.password, 10);
+        }
+
+        const user = await updateUserTableRows('users', req.user.id, allowedUpdates, req.body);
+
+        res.send(getPublicUser(users));
+    } catch (e) {
+        res.status(400).send({ error: e.message });
+    }
+})
+
+// Delete a user
+router.delete('/', auth, async (req, res) => {
+    try {
+        const deleteUserStatement = `
+        DELETE FROM users
+        WHERE id = $1
+        RETURNING *
+        `
+        const { rows: [users] } = await query(deleteUserStatement, [req.user.id]);
+
+        if (!user) {
+            return res.status(404).send({ error: 'Could not find user with that id' });
+        }
+        res.send(getPublicUser(user));
+
+    } catch (e) {
+        res.status(400).send({ error: e.message });
+    }
+})
---
+++ backend/utils.js
@@ -0,0 +1,37 @@
+
+const updateTableRow = async (table, id, allowedUpdates, updatesObj) => {
+    if (allowedUpdates.length < 1) {
+      throw new Error('No updates allowed')
+    }
+    const updateFields = Object.keys(updatesObj)
+    const validUpdate = updateFields.every((updateField) => allowedUpdates.includes(updateField))
+    if (!validUpdate) {
+      const fieldsString = allowedUpdates.length > 1
+        ? allowedUpdates.slice(0, allowedUpdates.length - 1).join(', ') + ` and ${allowedUpdates[allowedUpdates.length - 1]}`
+        : allowedUpdates[0]
+      throw new Error(`Only the ${fieldsString} ${allowedUpdates.length === 1 ? 'field' : 'fields'} can be updated`)
+    }
+  
+    const setFieldsClause = updateFields.map(
+      (updateField, index) => `${updateField} = $${index + 1}`
+    ).join(', ')
+  
+    const updateFieldsParams = []
+    updateFields.forEach((updateField) => {
+      updateFieldsParams.push(updatesObj[updateField])
+    })
+  
+    const updateTableStatement = `
+      update ${table}
+      set ${setFieldsClause}
+      where id = $${updateFields.length + 1}
+      returning *
+    `
+  
+    const { rows: [updatedRow] } = await query(updateTableStatement, [...updateFieldsParams, id])
+    return updatedRow
+}
+
+module.exports = {
+    updateTableRow
+}
